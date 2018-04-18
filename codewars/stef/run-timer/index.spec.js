ec995443d65c5db91fb598e78a05afeade9a3ebc
Stefanie Wehrle <steffinchen@gmail.com>
2018-04-18 17:10:12 +0100

    using sinon again (mostly), some beautification

--- codewars/stef/run-timer/index.spec.js
+++ codewars/stef/run-timer/index.spec.js
@@ -1,85 +1,93 @@
-const { shallow, mount } = require('enzyme');
-//const {spy} = require('sinon');
+const { shallow, mount } = require("enzyme");
+const sinon = require("sinon");
 
-
-describe('Component shallow tests', () => {
+describe("Component shallow tests", () => {
   let comp;
 
   before(() => {
     comp = shallow(<RunTimer />);
   });
-  
-  it('has a `componentWillMount` function', () => {
-    Test.assertEquals(comp.instance().componentWillMount.name, 'componentWillMount');
+
+  it("has a `componentWillMount` function", () => {
+    Test.assertEquals(
+      comp.instance().componentWillMount.name,
+      "componentWillMount"
+    );
   });
-  
-  it('has a `componentDidMount` function', () => {
-    Test.assertEquals(comp.instance().componentDidMount.name, 'componentDidMount');
+
+  it("has a `componentDidMount` function", () => {
+    Test.assertEquals(
+      comp.instance().componentDidMount.name,
+      "componentDidMount"
+    );
   });
-  
-  it('has a `componentWillUnmount` function', () => {
-    Test.assertEquals(comp.instance().componentWillUnmount.name, 'componentWillUnmount');
+
+  it("has a `componentWillUnmount` function", () => {
+    Test.assertEquals(
+      comp.instance().componentWillUnmount.name,
+      "componentWillUnmount"
+    );
   });
-  
-  it('has an initial count of 0', () => {
-    Test.assertEquals(comp.state('count'), 0);
+
+  it("has an initial count of 0", () => {
+    Test.assertEquals(comp.state("count"), 0);
   });
 });
 
-describe('Component mount tests', () => {
+describe("Component mount tests", () => {
   let comp;
 
   before(() => {
     comp = mount(<RunTimer />);
   });
-  
-  it('creates a timer and stores a reference to it in the state as timerId', () => {
-    Test.assertNotEquals(comp.state('timerId'), undefined);
+
+  it("creates a timer and stores a reference to it in the state as timerId", () => {
+    Test.assertNotEquals(comp.state("timerId"), undefined);
   });
-  
-  it('has a timer with interval of 1000ms', () => {
-    Test.assertEquals(comp.state('timerId')._idleTimeout, 1000);
+
+  it("has a timer with interval of 1000ms", () => {
+    Test.assertEquals(comp.state("timerId")._idleTimeout, 1000);
   });
-  
-  it('sets up timer in componentWillMount', () => {
-    let spy = {
-      called: false
-    };
-    setInterval = () => {
-      spy.called = true;
-    };
+
+  it("sets up timer in componentWillMount", () => {
+    let spy = sinon.spy(global, "setInterval");
     comp.instance().componentWillMount();
     Test.assertEquals(spy.called, true);
   });
-  
-  it('cancels the timer in componentWillUnmount', () => {
-   // let spy = sinon.spy(global, "clearInterval");
-    let spy = {
-      called: false
-    };
-    clearInterval = (timerId)=> {
-      spy.called = true;
-    };
+
+  it("cancels the timer in componentWillUnmount", () => {
+    let spy = sinon.spy(global, "clearInterval");
     comp.unmount();
     Test.assertEquals(spy.called, true);
   });
-  
-    it('fetches data in componentDidMount', () => {
+
+  it("fetches data in componentDidMount", done => {
     let spy = {
       called: false
     };
     fetch = async () => {
       spy.called = true;
-      return 'some stats';
+      return "some stats";
     };
+    // let spy = sinon.spy(fetch);
     comp.instance().componentDidMount();
-    Test.assertEquals(spy.called, true);
-  });
-  
-  it('fetches some data in componentDidMount', () => {
-    fetch().then(() => {
-      Test.assertEquals(comp.state('timedStats'), 'some stats', 'state.timedStats should hold the response' );
-    });
+    // setTimeout(function () {
+    Test.assertEquals(
+      spy.called,
+      true,
+      "fetch() should be called in componentDidMount"
+    );
+    //  done();
+    // }, 0);
   });
 
+  it("fetches some data in componentDidMount", () => {
+    fetch().then(() => {
+      Test.assertEquals(
+        comp.state("timedStats"),
+        "some stats",
+        "state.timedStats should hold the response"
+      );
+    });
+  });
 });
