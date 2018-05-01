5ef32a0b31b37fb1d182b3e381f484842634497e
Rob Adams <radams@findmypast.com>
2018-05-01 17:29:49 +0100

    Adding Packages

---
+++ codewars/test-setup/jsdom.js
@@ -0,0 +1,18 @@
+import { JSDOM } from 'jsdom';
+import enzyme, { shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+enzyme.configure({ adapter: new Adapter() });
+
+global.render = render;
+global.shallow = shallow;
+global.mount = mount;
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
